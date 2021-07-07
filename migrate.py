7fa5e1adc04854ba1807fd6503f96a8116b12a82
Tou <51247539+Touexe@users.noreply.github.com>
2021-07-07 13:16:19 +0700

    Update migrate.py

--- migrate.py
+++ migrate.py
@@ -5,14 +5,20 @@ apiHash = "" string
 oldChat = "" #chat name in string or chat id in interger
 newChat = "" #chat name in string or chat id in interger
 app = Client(
-    "Migrate",
+    "tgMigrate",
     api_id=,
     api_hash=)
 
 
 with app:
+    forwarded_count = 0
     for message in app.iter_history(oldChat, reverse = True):
+        if message.service == True:
+            continue   
         app.forward_messages(
             chat_id=newChat,
             from_chat_id=message.chat.id,
             message_ids=message.message_id)
+        forwarded_count += 1
+        print(f"[+] forward message {message.message_id} ({forwarded_count} forwarded)", end="\r")
+    print(f"\n\n[+] Done! ")
